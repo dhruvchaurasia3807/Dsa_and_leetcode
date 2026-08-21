3d468e10e5f473ef728d996aa77628436e584392
Dhruv Chaurasia <dhruvchaurasia3807@gmail.com>
2026-08-21 22:46:20 +0530

    convert string into uppercase and lowercase

---
+++ DSA/Level1/String/upper_lower_case.java
@@ -0,0 +1,11 @@
+package DSA.Level1.String;
+class upper_lower_case{
+    public static void main(String[] args) {
+        String str = "AmAn";
+        String result1 = str.toUpperCase();
+        String result2 = str.toLowerCase();
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
