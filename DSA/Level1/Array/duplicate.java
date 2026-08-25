9a0514185f81de22e2f56528e9091b5b22af58dc
Dhruv Chaurasia <dhruvchaurasia3807@gmail.com>
2026-08-25 21:48:07 +0530

    duplicate in an array

---
+++ DSA/Level1/Array/duplicate.java
@@ -0,0 +1,16 @@
+package DSA.Level1.Array;
+
+public class duplicate {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,2,4};
+        for(int i=0;i<arr.length;i++){
+            for(int j = i+1;j<arr.length;j++){
+                if(arr[i] == arr[j]){
+                    System.out.println("duplicate element is = "+arr[i]);
+                }
+            }
+        }
+    }
+}
+
+// output:duplicate element is = 2
