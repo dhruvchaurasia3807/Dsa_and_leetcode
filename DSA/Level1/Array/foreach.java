2fbbfb6e477cf53162a5e9977363296294f7062c
Dhruv Chaurasia <dhruvchaurasia3807@gmail.com>
2026-09-02 23:41:46 +0530

    for each loop in java

---
+++ DSA/Level1/Array/foreach.java
@@ -0,0 +1,15 @@
+package DSA.Level1.Array;
+
+public class foreach {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+
+        for (int item : arr) {
+            System.out.print(item + " ");
+        }
+    }
+
+}
+
+
+// output:1 2 3 4 5 
