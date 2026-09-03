dd890a7e16d7ec214b23aed43715238e029a6bb5
Dhruv Chaurasia <dhruvchaurasia3807@gmail.com>
2026-09-03 22:51:21 +0530

    count max score optimized code

---
+++ Leetcode/leetcode_easy/leet1422Third.java
@@ -0,0 +1,37 @@
+public class leet1422Third {
+
+    public static int maxScore(String s) {
+
+        int n = s.length();
+        int result = Integer.MIN_VALUE;
+
+        int zeros = 0;
+        int ones = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            if (s.charAt(i) == '1') {
+                ones++;
+            } else {
+                zeros++;
+            }
+
+            result = Math.max(result, zeros - ones);
+        }
+
+        if (s.charAt(n - 1) == '1') {
+            ones++;
+        }
+
+        return result + ones;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "011101";
+
+        int answer = maxScore(s);
+
+        System.out.println("Maximum Score = " + answer);
+    }
+}
