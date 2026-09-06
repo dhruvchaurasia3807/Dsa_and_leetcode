7378bd1728662f48e597a317a977213661975226
Dhruv Chaurasia <dhruvchaurasia3807@gmail.com>
2026-09-06 23:08:12 +0530

    saprate even and odd from an array

---
+++ DSA/Level1/Stage7_TwoPointers/saprateEven_odd.java
@@ -0,0 +1,31 @@
+package DSA.Level1.Stage7_TwoPointers;
+
+public class saprateEven_odd {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int left = 0;
+        int right = arr.length-1;
+        
+        while(left<right){
+
+            if(arr[left]%2==0){
+                left++;
+            }
+            else if(arr[right]%2!=0){
+                right--;
+            }
+            else{
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+}
