fc3b74529a64f95677dc7770551a4129ee8e4d59
Dhruv Chaurasia <dhruvchaurasia3807@gmail.com>
2026-09-15 23:12:22 +0530

    create and print aray list

---
+++ java_collection_freamwork/ArrayList/crateArrayList.java
@@ -0,0 +1,14 @@
+package java_collection_freamwork.ArrayList;
+import java.util.ArrayList;
+public class crateArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+    }
+}
+
+// output:[10, 20, 30]
