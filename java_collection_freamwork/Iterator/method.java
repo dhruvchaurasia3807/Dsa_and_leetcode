9dc2c514aef334f752273dd6a6299c2485b803aa
Dhruv Chaurasia <dhruvchaurasia3807@gmail.com>
2026-09-25 22:37:29 +0530

    method in iterator

---
+++ java_collection_freamwork/Iterator/method.java
@@ -0,0 +1,27 @@
+package java_collection_freamwork.Iterator;
+
+import java.util.*;
+
+public class method {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+
+        Iterator<Integer> it = list.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
+
+// output:
+// 10
+// 20
+// 30
+// 40
